ac6fac865a75c8998ba988af49ef8b04a676018d
Cojomax99 <cojomax99@gmail.com>
2013-03-29 20:43:57 -0400

    Added current time getters

--- cojo/clocks/time/TimeManager.java
+++ cojo/clocks/time/TimeManager.java
@@ -8,6 +8,34 @@ public class TimeManager {
 		
 	}
 	
+	public String getCurrentTime24() {
+		return getCurrentTime(24);
+	}
+	
+	public String getCurrentTime12() {
+		return getCurrentTime(12);
+	}
+	
+	private String getCurrentTime(int mode) {
+		Calendar calendar = Calendar.getInstance();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		if (mode == 24)
+			sb.append(calendar.get(Calendar.HOUR_OF_DAY));
+		else
+			if (mode == 12)
+				sb.append(calendar.get(Calendar.HOUR_OF_DAY));
+			else
+				throw new IllegalArgumentException("Illegal Hour mode");
+		sb.append(":");
+		sb.append(calendar.get(Calendar.MINUTE));
+		sb.append(":");
+		sb.append(calendar.get(Calendar.SECOND));
+		
+		return sb.toString();
+	}
+	
 	public String getCurrentDate() {
 		Calendar calendar = Calendar.getInstance();
 		
