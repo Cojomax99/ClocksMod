f98defbd4c8f98d8d38caab92df8fb411469c642
Cojomax99 <cojomax99@gmail.com>
2013-03-29 20:28:15 -0400

    Committing because I need to

---
+++ cojo/clocks/time/TimeManager.java
@@ -0,0 +1,19 @@
+package cojo.clocks.time;
+
+import java.util.Calendar;
+
+public class TimeManager {
+
+	public TimeManager() {
+		
+	}
+	
+	public String getCurrentDate() {
+		Calendar calendar = Calendar.getInstance();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(b)
+	}
+
+}
