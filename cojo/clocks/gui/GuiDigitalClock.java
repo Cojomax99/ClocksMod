437fe4efe902ab37045f8c694a3eb74a86a39292
Cojomax99 <cojomax99@gmail.com>
2013-03-30 22:27:11 -0400

    Welcome back

--- cojo/clocks/gui/GuiDigitalClock.java
+++ cojo/clocks/gui/GuiDigitalClock.java
@@ -14,9 +14,11 @@ public class GuiDigitalClock extends GuiScreen {
 	
     /** Text field containing the clock's text color. */
     private GuiTextField colorTextField;
+    
+    private TileEntityDigitalClock clock;
 	
 	public GuiDigitalClock(TileEntityDigitalClock clock) {
-		
+		this.clock = clock;
 	}
 	
     /**
@@ -34,6 +36,10 @@ public class GuiDigitalClock extends GuiScreen {
     {
     	this.buttonList.clear();
         this.buttonList.add(this.butt1 = new GuiButton(0, this.width / 2 - 100, this.height / 4 + 96 + 12, ": )"));
+        this.colorTextField = new GuiTextField(this.fontRenderer, this.width / 2 - 150, 60, 300, 20);
+        this.colorTextField.setMaxStringLength(32767);
+        this.colorTextField.setFocused(true);
+        this.colorTextField.setText(this.clock.getColor() + "");
     }
 	
     /**
@@ -41,7 +47,25 @@ public class GuiDigitalClock extends GuiScreen {
      */
     public void drawScreen(int par1, int par2, float par3)
     {
+    	this.colorTextField.drawTextBox();
         super.drawScreen(par1, par2, par3);
+    }
+    
+    /**
+     * Called when the mouse is clicked.
+     */
+    protected void mouseClicked(int par1, int par2, int par3)
+    {
+        super.mouseClicked(par1, par2, par3);
+        this.colorTextField.mouseClicked(par1, par2, par3);
+    }
+    
+    /**
+     * Fired when a key is typed. This is the equivalent of KeyListener.keyTyped(KeyEvent e).
+     */
+    protected void keyTyped(char par1, int par2)
+    {
+        this.colorTextField.textboxKeyTyped(par1, par2);
     }
 
 }
